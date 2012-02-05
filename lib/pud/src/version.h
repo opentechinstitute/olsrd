4532e337fd252737e259baacbb93d48b6aa18aad
Ferry Huberts <ferry.huberts@pelagic.nl>
2012-02-05 13:45:25 +0100

    PUD: only update version.h when needed
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- lib/pud/src/version.h
+++ lib/pud/src/version.h
@@ -1,7 +1,7 @@
 #ifndef _PUD_VERSION_H_
 #define _PUD_VERSION_H_
 
-#define PLUGIN_VER "1.0.0"
+#define PLUGIN_VER "1.1.0"
 #define GIT_SHA    "OLSRD_Unknown-g0000000"
 
 #endif /* _PUD_VERSION_H_ */
