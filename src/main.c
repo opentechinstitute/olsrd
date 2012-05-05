8ae0063bc82827204367fc8bc49eef7af7cf1b8b
Henning Rogge <hrogge@googlemail.com>
2012-05-05 13:05:46 +0200

    Use __ANDROID__ instead of "android" for preprocessor

--- src/main.c
+++ src/main.c
@@ -80,7 +80,7 @@ bool olsr_win32_end_flag = false;
 static void olsr_shutdown(int) __attribute__ ((noreturn));
 #endif
 
-#if defined android
+#if defined __ANDROID__
 #define DEFAULT_LOCKFILE_PREFIX "/data/local/olsrd"
 #elif defined linux || __FreeBSD__ || defined __NetBSD__ || defined __OpenBSD__
 #define DEFAULT_LOCKFILE_PREFIX "/var/run/olsrd"
