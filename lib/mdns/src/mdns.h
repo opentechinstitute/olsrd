00818ac8d3ffb5370b7f6f1fc2bc82f4894f8681
Saverio Proto <zioproto@gmail.com>
2012-05-30 19:18:45 +0200

    mDNS: Change version of mdns plugin to 1.0.1 after GSoC 2012 work by Alessandro Gnagni

--- lib/mdns/src/mdns.h
+++ lib/mdns/src/mdns.h
@@ -57,7 +57,7 @@
 /* BMF plugin data */
 #define PLUGIN_NAME "OLSRD MDNS plugin"
 #define PLUGIN_NAME_SHORT "OLSRD MDNS"
-#define PLUGIN_VERSION "1.0.0 (" __DATE__ " " __TIME__ ")"
+#define PLUGIN_VERSION "1.0.1 (" __DATE__ " " __TIME__ ")"
 #define PLUGIN_COPYRIGHT "  (C) Ninux.org"
 #define PLUGIN_AUTHOR "  Saverio Proto (zioproto@gmail.com)"
 #define MOD_DESC PLUGIN_NAME " " PLUGIN_VERSION "\n" PLUGIN_COPYRIGHT "\n" PLUGIN_AUTHOR
