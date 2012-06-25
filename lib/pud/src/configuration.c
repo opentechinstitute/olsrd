opentechinstitute/olsrd

commit 63e28a08147bdbee6aacea6327c97a047bd756c3

PUD: remove usage of getifaddrs
Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

lib/pud/src/configuration.c
@@ -1412,52 +1412,5 @@ unsigned int checkConfig(void) {
  - false otherwise
  */
 unsigned int checkRunSetup(void) {
-	int retval = true;
-	unsigned int i;
-
-	/* any receive interface name that is configured but is not the name of an
-	 * actual receive interface is not a valid interface name */
-	for (i = 0; i < rxNonOlsrInterfaceCount; i++) {
-		unsigned char * nonOlsrInterfaceName = &rxNonOlsrInterfaceNames[i][0];
-
-		TRxTxNetworkInterface * interfaceObject = getRxNetworkInterfaces();
-		bool found = false;
-		while (interfaceObject != NULL) {
-			if (strncmp((char *) nonOlsrInterfaceName,
-					(char *) &interfaceObject->name[0], IFNAMSIZ + 1) == 0) {
-				found = true;
-				break;
-			}
-			interfaceObject = interfaceObject->next;
-		}
-		if (!found) {
-			pudError(false, "Configured receive non-OLSR interface %s is not"
-				" a known interface name", nonOlsrInterfaceName);
-			retval = false;
-		}
-	}
-
-	/* any transmit interface name that is configured but is not the name of an
-	 * actual transmit interface is not a valid interface name */
-	for (i = 0; i < txNonOlsrInterfaceCount; i++) {
-		unsigned char * nonOlsrInterfaceName = &txNonOlsrInterfaceNames[i][0];
-
-		TRxTxNetworkInterface * interfaceObject = getTxNetworkInterfaces();
-		bool found = false;
-		while (interfaceObject != NULL) {
-			if (strncmp((char *) nonOlsrInterfaceName,
-					(char *) &interfaceObject->name[0], IFNAMSIZ + 1) == 0) {
-				found = true;
-				break;
-			}
-			interfaceObject = interfaceObject->next;
-		}
-		if (!found) {
-			pudError(false, "Configured transmit non-OLSR interface %s is not"
-				" a known interface name", nonOlsrInterfaceName);
-			retval = false;
-		}
-	}
-
-	return retval;
+	return true;
 }

lib/pud/src/networkInterfaces.c
@@ -12,7 +12,6 @@
 /* System includes */
 #include <unistd.h>
 #include <fcntl.h>
-#include <ifaddrs.h>
 
 /*
  * Main IP MAC address
@@ -590,61 +589,45 @@ static int createDownlinkSocket(int ipVersion, socket_handler_func rxSocketHandl
  */
 bool createNetworkInterfaces(socket_handler_func rxSocketHandlerFunction,
 		socket_handler_func rxSocketHandlerFunctionDownlink) {
-	int retval = false;
-	struct ifaddrs *ifAddrs = NULL;
-	struct ifaddrs *ifAddr = NULL;
+	TRxTxNetworkInterface * interfaces;
 	union olsr_sockaddr * rxMcAddr = getRxMcAddr();
 	union olsr_sockaddr * txMcAddr = getTxMcAddr();
 
-	errno = 0;
-	if (getifaddrs(&ifAddrs) != 0) {
-		pudError(true, "Could not get list of interfaces and their addresses");
-		return retval;
-	}
-
-	/* loop over all interfaces */
-	for (ifAddr = ifAddrs; ifAddr != NULL; ifAddr = ifAddr->ifa_next) {
-		union olsr_sockaddr * addr = (union olsr_sockaddr *)ifAddr->ifa_addr;
-		if ((addr != NULL) && ((addr->in.sa_family == AF_INET) || (addr->in.sa_family == AF_INET6))) {
-			char * ifName = ifAddr->ifa_name;
-
-			/* determine whether the iterated interface is configured as a
-			 * non-OLSR interface in the plugin parameter list */
-			bool isRxNonOlsrIf = isRxNonOlsrInterface(ifName) && (addr->in.sa_family == rxMcAddr->in.sa_family);
-			bool isTxNonOlsrIf = isTxNonOlsrInterface(ifName) && (addr->in.sa_family == txMcAddr->in.sa_family);
-
-			bool isNonOlsrIf = isRxNonOlsrIf || isTxNonOlsrIf;
-
-			if (!isNonOlsrIf) {
-				/* interface is not configured as non-OLSR interface: skip */
-				continue;
-			}
-
-			if (isRxNonOlsrIf && !createRxInterface(ifName, rxSocketHandlerFunction, rxMcAddr)) {
+	/* loop over all configured rx interfaces */
+	interfaces = getRxNetworkInterfaces();
+	if (interfaces) {
+		TRxTxNetworkInterface * interface;
+		for (interface = interfaces; interface != NULL; interface = interfaces->next) {
+			if (!createRxInterface(interface->name, rxSocketHandlerFunction, rxMcAddr)) {
 				/* creating a receive interface failed */
-				goto end;
-			}
-
-			if (isTxNonOlsrIf && !createTxInterface(ifName, txMcAddr)) {
-				/* creating a transmit interface failed */
-				goto end;
+				return false;
 			}
 		}
 	}
 
+	/* loop over all configured tx interfaces */
+	interfaces = getTxNetworkInterfaces();
+	if (interfaces) {
+		TRxTxNetworkInterface * interface;
+		for (interface = interfaces; interface != NULL; interface = interfaces->next) {
+			if (!createTxInterface(interface->name, txMcAddr)) {
+				/* creating a transmit interface failed */
+				return false;
+			}
+		}
+	}
+
+	/* create uplink socket when needed */
 	if (isUplinkAddrSet()) {
 		downlinkSocketFd = createDownlinkSocket(getUplinkAddr()->in.sa_family, rxSocketHandlerFunctionDownlink);
 		if (downlinkSocketFd == -1) {
-			goto end;
+			return false;
 		}
 	} else {
 		downlinkSocketFd = -1;
 	}
 
-	retval = true;
-
-	end: freeifaddrs(ifAddrs);
-	return retval;
+	return true;
 }
 
 /**

commit 7cdf132893dd930cc305c330a0cf77544858e7ee

PUD: fix setting up network interface.
Broke in 63e28a0

Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

lib/pud/src/configuration.c
@@ -359,6 +359,21 @@ int addRxNonOlsrInterface(const char *value, void *data __attribute__ ((unused))
 	return false;
 }
 
+/**
+ * @return the number of configured non-olsr receive interfaces
+ */
+unsigned int getRxNonOlsrInterfaceCount(void) {
+	return rxNonOlsrInterfaceCount;
+}
+
+/**
+ * @param index the index of the configured non-olsr receive interface
+ * @return the index-th interface name
+ */
+unsigned char * getRxNonOlsrInterfaceName(unsigned int index) {
+	return &rxNonOlsrInterfaceNames[index][0];
+}
+
 /*
  * rxAllowedSourceIpAddress
  */
@@ -614,6 +629,21 @@ int addTxNonOlsrInterface(const char *value, void *data __attribute__ ((unused))
 	}
 
 	return false;
+}
+
+/**
+ * @return the number of configured non-olsr transmit interfaces
+ */
+unsigned int getTxNonOlsrInterfaceCount(void) {
+	return txNonOlsrInterfaceCount;
+}
+
+/**
+ * @param index the index of the configured non-olsr transmit interface
+ * @return the index-th interface name
+ */
+unsigned char * getTxNonOlsrInterfaceName(unsigned int index) {
+	return &txNonOlsrInterfaceNames[index][0];
 }
 
 /*

lib/pud/src/configuration.h
@@ -44,6 +44,8 @@ int setNodeId(const char *value, void *data, set_plugin_parameter_addon addon);
 bool isRxNonOlsrInterface(const char *ifName);
 int addRxNonOlsrInterface(const char *value, void *data,
 		set_plugin_parameter_addon addon);
+unsigned int getRxNonOlsrInterfaceCount(void);
+unsigned char * getRxNonOlsrInterfaceName(unsigned int index);
 
 /** The name of the allowed source IP address plugin parameter */
 #define PUD_RX_ALLOWED_SOURCE_IP_NAME			"rxAllowedSourceIpAddress"
@@ -91,6 +93,8 @@ int setPositionFile(const char *value, void *data, set_plugin_parameter_addon ad
 bool isTxNonOlsrInterface(const char *ifName);
 int addTxNonOlsrInterface(const char *value, void *data,
 		set_plugin_parameter_addon addon);
+unsigned int getTxNonOlsrInterfaceCount(void);
+unsigned char * getTxNonOlsrInterfaceName(unsigned int index);
 
 /** The name of the transmit multicast address plugin parameter */
 #define PUD_TX_MC_ADDR_NAME						"txMcAddr"

lib/pud/src/netTools.c
@@ -51,3 +51,43 @@ unsigned char * getHardwareAddress(const char * ifName, int family,
 
 	return (unsigned char *) &ifr->ifr_hwaddr.sa_data[0];
 }
+
+/**
+ Get the IPv4 address of an interface
+
+ @param ifName
+ the name of the interface
+ @param ifr
+ the buffer in which to write the IPv4 address
+
+ @return
+ - the pointer to the IPv4 address (inside ifr)
+ - NULL on failure
+ */
+struct in_addr * getIPv4Address(const char * ifName, struct ifreq *ifr) {
+	int fd;
+	int cpySize;
+
+	assert(ifName != NULL);
+	assert(strlen(ifName) <= IFNAMSIZ);
+	assert(ifr != NULL);
+
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	ifr->ifr_addr.sa_family = AF_INET;
+	memset(ifr->ifr_name, 0, sizeof(ifr->ifr_name));
+	cpySize = (strlen(ifName) < sizeof(ifr->ifr_name)) ? strlen(ifName)
+			: sizeof(ifr->ifr_name);
+	strncpy(ifr->ifr_name, ifName, cpySize);
+
+	errno = 0;
+	if (ioctl(fd, SIOCGIFADDR, ifr) < 0) {
+		pudError(true, "%s@%u: ioctl(SIOCGIFADDR) error", __FILE__, __LINE__);
+		close(fd);
+		return NULL;
+	}
+
+	close(fd);
+
+	return &((struct sockaddr_in *) &ifr->ifr_addr)->sin_addr;
+}

lib/pud/src/netTools.h
@@ -83,4 +83,6 @@ static inline bool isMulticast(union olsr_sockaddr *addr) {
 unsigned char * getHardwareAddress(const char * ifName, int family,
 		struct ifreq *ifr);
 
+struct in_addr * getIPv4Address(const char * ifName, struct ifreq *ifr);
+
 #endif /* _PUD_NETTOOLS_H_ */

lib/pud/src/networkInterfaces.c
@@ -175,16 +175,17 @@ static int createRxSocket(TRxTxNetworkInterface * networkInterface,
 	 * which the multicast datagrams are to be received. */
 	if (ipFamilySetting == AF_INET) {
 		struct ip_mreq mc_settings;
-		struct interface* interface = if_ifwithname(networkInterface->name);
 
-		if (!interface) {
+		struct ifreq ifr;
+		struct in_addr * ifAddr = getIPv4Address(networkInterface->name, &ifr);
+		if (!ifAddr) {
 			pudError(true, "Could not get interface address of %s", networkInterface->name);
 			goto bail;
 		}
 
 		(void) memset(&mc_settings, 0, sizeof(mc_settings));
 		mc_settings.imr_multiaddr = rxMcAddr->in4.sin_addr;
-		mc_settings.imr_interface = interface->int_addr.sin_addr;
+		mc_settings.imr_interface = *ifAddr;
 		errno = 0;
 		if (setsockopt(rxSocket, ipProtoSetting, ipAddMembershipSetting,
 				&mc_settings, sizeof(mc_settings)) < 0) {
@@ -333,9 +334,9 @@ static int createTxSocket(TRxTxNetworkInterface * networkInterface, union olsr_s
 
 	memset(&address, 0, sizeof(address));
 	if (txMcAddr->in.sa_family == AF_INET) {
-		struct interface* interface = if_ifwithname(networkInterface->name);
-
-		if (!interface) {
+		struct ifreq ifr;
+		struct in_addr * ifAddr = getIPv4Address(networkInterface->name, &ifr);
+		if (!ifAddr) {
 			pudError(true, "Could not get interface address of %s", networkInterface->name);
 			goto bail;
 		}
@@ -350,7 +351,7 @@ static int createTxSocket(TRxTxNetworkInterface * networkInterface, union olsr_s
 		ifIndex = 0;
 
 		address.in4.sin_family = ipFamilySetting;
-		address.in4.sin_addr = interface->int_addr.sin_addr;
+		address.in4.sin_addr = *ifAddr;
 		address.in4.sin_port = getTxMcPort();
 		addr = &address.in4;
 		addrSize = sizeof(struct sockaddr_in);
@@ -589,31 +590,25 @@ static int createDownlinkSocket(int ipVersion, socket_handler_func rxSocketHandl
  */
 bool createNetworkInterfaces(socket_handler_func rxSocketHandlerFunction,
 		socket_handler_func rxSocketHandlerFunctionDownlink) {
-	TRxTxNetworkInterface * interfaces;
 	union olsr_sockaddr * rxMcAddr = getRxMcAddr();
 	union olsr_sockaddr * txMcAddr = getTxMcAddr();
+	unsigned int count = 0;
 
 	/* loop over all configured rx interfaces */
-	interfaces = getRxNetworkInterfaces();
-	if (interfaces) {
-		TRxTxNetworkInterface * interface;
-		for (interface = interfaces; interface != NULL; interface = interfaces->next) {
-			if (!createRxInterface(interface->name, rxSocketHandlerFunction, rxMcAddr)) {
-				/* creating a receive interface failed */
-				return false;
-			}
+	count = getRxNonOlsrInterfaceCount();
+	while (count--) {
+		if (!createRxInterface((char *)getRxNonOlsrInterfaceName(count), rxSocketHandlerFunction, rxMcAddr)) {
+			/* creating a receive interface failed */
+			return false;
 		}
 	}
 
 	/* loop over all configured tx interfaces */
-	interfaces = getTxNetworkInterfaces();
-	if (interfaces) {
-		TRxTxNetworkInterface * interface;
-		for (interface = interfaces; interface != NULL; interface = interfaces->next) {
-			if (!createTxInterface(interface->name, txMcAddr)) {
-				/* creating a transmit interface failed */
-				return false;
-			}
+	count = getTxNonOlsrInterfaceCount();
+	while (count--) {
+		if (!createTxInterface((char *)getTxNonOlsrInterfaceName(count), txMcAddr)) {
+			/* creating a transmit interface failed */
+			return false;
 		}
 	}
 