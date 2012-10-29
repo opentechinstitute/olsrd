ddc29d32092fcfd7277af058e33ecb9c9e736b9d
Henning Rogge <henning.rogge@fkie.fraunhofer.de>
2012-10-29 14:37:37 +0100

    Remove dead codepath
    CID 739641: Logically dead code (DEADCODE)
    Execution cannot reach this expression "NULL" inside statement "err = olsr_new_netlink_rout...".

--- src/common/avl.c
+++ src/common/avl.c
@@ -508,10 +508,8 @@ avl_delete_worker(struct avl_tree *tree, struct avl_node *node)
       avl_rotate_right(tree, parent->right);
       avl_rotate_left(tree, parent);
       avl_post_delete(tree, parent->parent);
-      return;
     }
-
-    if (parent->right == node) {
+    else {
       parent->right = NULL;
       parent->balance--;
 
@@ -537,8 +535,8 @@ avl_delete_worker(struct avl_tree *tree, struct avl_node *node)
       avl_rotate_left(tree, parent->left);
       avl_rotate_right(tree, parent);
       avl_post_delete(tree, parent->parent);
-      return;
     }
+    return;
   }
 
   if (node->left == NULL) {
