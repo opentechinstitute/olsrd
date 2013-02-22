opentechinstitute/olsrd

commit b3f05653fc085193504218e47458891acf7c495d

Check inet_pton return value
    Coverity:

    CID 739622 (#1 of 1): Unchecked return value (CHECKED_RETURN)
    At (8): Calling function "inet_pton(int, char const * restrict, void * restrict)" without checking return value (as is done elsewhere 22 out of 25 times).

    Signed-off-by: Henning Rogge <henning.rogge@fkie.fraunhofer.de>

src/linux/net.c
@@ -642,7 +642,9 @@ get_ipv6_address(char *ifname, struct sockaddr_in6 *saddr6, struct olsr_ip_prefi
         OLSR_PRINTF(5, "\tinet6 addr: %s\n", addr6);
         OLSR_PRINTF(5, "\tScope: %d\n", scope);
 
-        inet_pton(AF_INET6, addr6, &tmp_ip.v6);
+        if (inet_pton(AF_INET6, addr6, &tmp_ip.v6)) {
+          continue;
+        }
 
         isNetWide = (scope != IPV6_ADDR_LOOPBACK) && (scope != IPV6_ADDR_LINKLOCAL) && (scope != IPV6_ADDR_SITELOCAL);
 

commit 247df1398d5b19e9583213f6f59f1696fedf4f91

main: fix the check on inet_pton return value because it returns 1 on success.
This bug was introduced in commit b3f05653fc085193504218e47458891acf7c495d and it was preventing olsrd from working in IPv6 mode.

src/linux/net.c
@@ -637,7 +637,7 @@ get_ipv6_address(char *ifname, struct sockaddr_in6 *saddr6, struct olsr_ip_prefi
         OLSR_PRINTF(5, "\tinet6 addr: %s\n", addr6);
         OLSR_PRINTF(5, "\tScope: %d\n", scope);
 
-        if (inet_pton(AF_INET6, addr6, &tmp_ip.v6)) {
+        if (inet_pton(AF_INET6, addr6, &tmp_ip.v6) != 1) {
           continue;
         }
 