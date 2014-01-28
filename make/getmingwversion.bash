4106a15bd1575f3ff026f886b9e92bd52c15c6ee
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-01-28 14:54:47 +0100

    mingw: fix Ubuntu 13.10 / Debian Wheezy mingw mess
    Ubuntu 13.10 and Debian Wheezy both report mingw 4.6.
    Debian Wheezy needs the old setup, while Ubuntu 13.10 needs the new setup:
    We decrement the version to 4.5 for Debian Wheezy
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- make/getmingwversion.bash
+++ make/getmingwversion.bash
@@ -23,5 +23,20 @@ if [ ${#versions[*]} -ne 3 ]; then
   versions=( 0 0 0 )
 fi
 
+# Ubuntu 13.10 and Debian Wheezy both report mingw 4.6.
+# - Debian Wheezy needs the old setup for 64 bits and the new setup for 32 bits.
+# - Ubuntu 13.10 needs the new setup for both 32 bits and 64 bits.
+# --> We decrement the version to 4.5 for Debian Wheezy 64 bits
+if [ "${versions[0]}" == "4" ] && \
+   [ "${versions[1]}" == "6" ] && \
+   [ "${versions[2]}" == "0" ]; then
+  detectDist="$(uname -a | grep -i debian)"
+  detectArch="$("$GCC" -dumpmachine | grep -i x86_64)"
+  if [ -n "$detectDist" ] && \
+     [ -n "$detectArch" ]; then
+    versions[1]=5
+  fi
+fi
+
 version=$(( versions[0]*10000 + versions[1]*100 + versions[2] ))
 echo "$version"
