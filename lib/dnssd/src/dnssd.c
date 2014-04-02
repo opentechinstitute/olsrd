0489132d8e1e570fec0b2f6f82b39bf3a8f50cb1
Dan Staples <dan.staples@riseup.net>
2014-04-02 14:39:54 -0400

    remove periodic mDNS querying

--- lib/dnssd/src/dnssd.c
+++ lib/dnssd/src/dnssd.c
@@ -1203,7 +1203,7 @@ InitP2pd(struct interface *skipThisIntf)
   //Creates captures sockets and register them to the OLSR scheduler
   CreateNonOlsrNetworkInterfaces(skipThisIntf);
   
-  AddInterface(olsr_cnf->interfaces->interf);
+//   AddInterface(olsr_cnf->interfaces->interf);
 
   olsr_start_timer(0, 0, OLSR_TIMER_ONESHOT, SetSignalHandler, NULL, 0);
   
@@ -1534,131 +1534,7 @@ int SetupServiceList(const char *value, void *data __attribute__ ((unused)), set
   // set timer for period ServiceList updating
   service_update_timer = olsr_start_timer(ServiceUpdateInterval * MSEC_PER_SEC, EMISSION_JITTER, OLSR_TIMER_PERIODIC, UpdateServices, NULL, 0);
   
-  // set timer for periodically prompting service announcements
-  service_query_timer = olsr_start_timer(SERVICE_QUERY_INTERVAL * MSEC_PER_SEC, EMISSION_JITTER, OLSR_TIMER_PERIODIC, PromptAnnouncements, NULL, 0);
-    
   return 0;
-}
-
-void PromptAnnouncements(void *context __attribute__((unused))) {
-  int ret;
-  const char dnssd_type[] = "_services._dns-sd._udp.mesh.local";
-  char srv_type[256];
-  uint8_t *pkt_buf = NULL;
-  size_t buf_size = 0;
-  struct MdnsService *service = NULL;
-  ldns_rdf *rdf = NULL;
-  ldns_rr *question_rr = NULL;
-  ldns_rr_list *rr_list = NULL;
-  ldns_pkt *pkt = NULL;
-  struct DnssdInterface *ifwalker = NULL;
-  struct UdpDestPort *walker = NULL;
-  union olsr_sockaddr addr;
-  
-  ret = ldns_pkt_query_new_frm_str(&pkt, dnssd_type, LDNS_RR_TYPE_PTR, LDNS_RR_CLASS_IN, 0);
-  if (ret != LDNS_STATUS_OK) {
-    P2pdPError("Failed to create ldns packet: %s\n", ldns_get_errorstr_by_id(ret));
-    if (pkt)
-      ldns_pkt_free(pkt);
-    return;
-  }
-  
-  for (service = ServiceList; service != NULL; service=service->hh.next) {
-    memset(srv_type, 0, 256);
-    strcpy(srv_type, service->service_type);
-    strcat(srv_type, ".mesh.local");
-    ret = ldns_str2rdf_dname(&rdf, srv_type);
-    if (ret != LDNS_STATUS_OK) {
-      P2pdPError("Failed to create rdf: %s\n", ldns_get_errorstr_by_id(ret));
-      if (rdf)
-	ldns_rdf_free(rdf);
-      ldns_pkt_free(pkt);
-      return;
-    }
-    
-    // Don't add duplicate RRs
-    if ((rr_list = ldns_pkt_rr_list_by_name(pkt, rdf, LDNS_SECTION_QUESTION))) {
-      ldns_rr_list_free(rr_list);
-      ldns_rdf_free(rdf);
-      continue;
-    }
-    
-    question_rr = ldns_rr_new();
-    if (!question_rr) {
-      P2pdPError("Failed to create rr");
-      ldns_rdf_free(rdf);
-      ldns_pkt_free(pkt);
-      return;
-    }
-    ldns_rr_set_owner(question_rr, rdf);
-    ldns_rr_set_type(question_rr, LDNS_RR_TYPE_PTR);
-    ldns_rr_set_class(question_rr, LDNS_RR_CLASS_IN);
-    ldns_rr_set_question(question_rr, 1);
-    ldns_pkt_push_rr(pkt, LDNS_SECTION_QUESTION, question_rr);
-  }
-  
-  // Convert packet to wire format
-  ret = ldns_pkt2wire(&pkt_buf, pkt, &buf_size);
-  if (ret != LDNS_STATUS_OK) {
-    P2pdPError("Error converting dns packet to wire format: %s\n", ldns_get_errorstr_by_id(ret));
-    ldns_pkt_free(pkt);
-    if (pkt_buf)
-      free(pkt_buf);
-    return;
-  }
-  ldns_pkt_free(pkt);
-  
-  // Send packet to local interface(s) and UDP destination addresses
-  for (ifwalker = OlsrInterfaces; ifwalker != NULL; ifwalker = ifwalker->next) {
-      
-      for (walker = UdpDestPortList; walker; walker = walker->next) {
-	int nBytesWritten;
-	
-	memset(&addr, 0, sizeof(union olsr_sockaddr));
-	
-// 	if (walker->ip_version == AF_INET) {
-	  addr.in4.sin_family = AF_INET;
-	  addr.in4.sin_port = htons(walker->port);
-	  addr.in4.sin_addr.s_addr = walker->address.v4.s_addr;
-	  
-	  nBytesWritten = sendto(ifwalker->ipSkfd,
-			       pkt_buf,
-			       buf_size,
-			       0,
-			       (struct sockaddr *) &addr.in4,
-			       sizeof(struct sockaddr));
-	  
-// 	} else /* ip_version == AF_INET6 */ {
-// 	  addr.in6.sin6_family = AF_INET;
-// 	  addr.in6.sin6_port = walker->port;
-// 	  memcpy(&addr.in6.sin6_addr.s6_addr, walker->address.v6.s6_addr, 16);
-// 	  
-// 	  nBytesWritten = sendto(ifwalker->ipSkfd,
-// 			       pkt_buf,
-// 			       buf_size,
-// 			       0,
-// 			       (struct sockaddr *) &addr.in6,
-// 			       sizeof(struct sockaddr));
-	  
-// 	}
-	
-	if (nBytesWritten != buf_size) {
-	  P2pdPError("sendto() error forwarding unpacked encapsulated pkt on \"%s\"",
-		     ifwalker->ifName);
-	} else {
-#ifdef INCLUDE_DEBUG_OUTPUT
-	  OLSR_PRINTF(
-	    2,
-	    "%s: Sent mDNS queries on \"%s\" to %s:%d\n",
-	    PLUGIN_NAME_SHORT,
-	    ifwalker->ifName,
-	    inet_ntoa(addr.in4.sin_addr),
-	    addr.in4.sin_port);
-#endif
-	}
-      }
-  }
-  free(pkt_buf);
 }
 
 /* -------------------------------------------------------------------------
