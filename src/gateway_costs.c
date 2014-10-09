35f8768018a4dc96b9e4e696e18caf0f44c40acd
Ferry Huberts <ferry.huberts@pelagic.nl>
2014-10-09 11:36:57 +0200

    sgw: cost calculation can now directly account for interface up/down status
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- src/gateway_costs.c
+++ src/gateway_costs.c
@@ -7,10 +7,15 @@ extern "C" {
 #define SCALING_SHIFT_CLASSIC 31
 #define SCALING_SHIFT 23
 
-int64_t gw_costs_weigh(const struct costs_weights weights, uint32_t path_cost, uint32_t exitUk, uint32_t exitDk) {
+int64_t gw_costs_weigh(bool up, const struct costs_weights weights, uint32_t path_cost, uint32_t exitUk, uint32_t exitDk) {
   int64_t costU;
   int64_t costD;
   int64_t costE;
+
+  if (!up) {
+    /* interface is down */
+    return INT64_MAX;
+  }
 
   if (!weights.Detx) {
     /* only consider path costs (classic behaviour) (but scale to 64 bit) */
